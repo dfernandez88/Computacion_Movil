9aeb93063fcdc73049c016436895f1d311a2ecb8
DaRi Fernandez <darifernandez17@gmail.com>
2016-04-12 23:58:08 -0500

    chat 1

---
+++ Proyecto_Android/DaTiChat/app/src/main/java/com/example/dati/datichat/RegistroActivity.java
@@ -0,0 +1,13 @@
+package com.example.dati.datichat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RegistroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.registrate);
+    }
+}
