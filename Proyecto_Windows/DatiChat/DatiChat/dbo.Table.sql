44094d4b9fdc4cad9fdb05919afadaf859154613
Dari Fernandez <darifernandez17@gmail.com>
2016-05-20 00:50:51 -0500

    >=D

--- Proyecto_Windows/DatiChat/DatiChat/dbo.Table.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Mensajes]
-(
-	[Id] INT NOT NULL PRIMARY KEY, 
-    [User_from] INT NOT NULL, 
-    [User_to] INT NOT NULL, 
-    [Message ] NVARCHAR(MAX) NOT NULL, 
-    [DateTime ] DATETIME NOT NULL
-)
